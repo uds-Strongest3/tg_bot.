f325d8709db9ca82a5160362168a8b6e40ee1155
uds-Strongest3 <ustd157@gmail.com>
2026-02-03 20:21:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import telebot
+import telebot, time, threading, schedule 
 from bot_logic import gen_pass
 
-token ="
+token =""
 
 bot = telebot.TeleBot(token)
     
@@ -15,8 +15,21 @@ def send_hello(message):
 def send_bye(message):
     bot.reply_to(message, "Пока! Удачи!")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-bot.polling()
+
+def beep(chat_id) -> None:
+    """Send the beep message."""
+    bot.send_message(chat_id, text='Beep!')
+
+@bot.message_handler(commands=['set'])
+def set_timer(message):
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
+    else:
+        bot.reply_to(message, 'Usage: /set <seconds>')
+
+@bot.message_handler(commands=['unset'])
+def unset_timer(message):
+    schedule.clear(message.chat.id)
 
