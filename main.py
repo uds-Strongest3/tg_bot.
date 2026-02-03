ee572755e735381e9271faf35eadf2e7a1619473
uds-Strongest3 <ustd157@gmail.com>
2026-02-03 17:40:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+from bot_logic import gen_pass
+
+token ="7979551876:AAEo56mAnf8TPpp41r9pGfcQaxa-Bsz9vXc"
+
+bot = telebot.TeleBot(token)
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+bot.polling()
+
